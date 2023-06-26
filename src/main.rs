9c85adf36ba8c0610cb96549826fa5f296582a7b
roesnera <adam.roesner@generation.org>
2023-06-26 17:04:53 -0400

    reads file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
     dbg!(&args);
@@ -6,4 +7,9 @@ fn main() {
     let location = &args[2];
 
     println!("Searching for {query} in {location}");
+
+    let contents = fs::read_to_string(location)
+        .expect("Should have been able to read the file!");
+
+    println!("With text:\n{contents}");
 }
